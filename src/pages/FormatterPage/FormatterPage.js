2c1b5a0bb12451b3456a68dba16ed5579cad4985
Illya Tsishkevich <relikt.ilya@gmail.com>
2026-06-14 23:38:16 +0300

    title/description

--- src/pages/FormatterPage/FormatterPage.js
+++ src/pages/FormatterPage/FormatterPage.js
@@ -46,7 +46,7 @@ const FormatterPage = () => {
             <link rel="canonical" href={ url } />
         </Helmet>
         <main className="py-3 px-5">
-            <h1 className="display-6 fw-normal mb-3">{ title }</h1>
+            <h1 className="display-6 fw-normal mb-3">{ `${outputFormat.toUpperCase()} Formatter` }</h1>
             <div className="d-flex flex-column flex-xl-row">
                 <div className="bg-body-tertiary border rounded-3 p-1 panel panel-input">
                     <Input />
