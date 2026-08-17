e13704ad855bd9162d5383cb2438c4ffdf45022d
Illya Tsishkevich <relikt.ilya@gmail.com>
2026-08-18 00:18:07 +0300

    add footer link page

---
+++ src/components/Footer/ConverterMenu.js
@@ -0,0 +1,37 @@
+import {Link} from "react-router-dom";
+import SUPPORTED from "../../utils/formats";
+import { firstToUpper } from "../../core/util";
+
+const ConverterMenu = () => {
+    const block = SUPPORTED.map((inputFormat) => <List inputFormat={inputFormat} />)
+
+    return <>
+        <div className="col mb-12 align-items-end text-end">
+            <h6>Conversion pages:</h6>
+        </div>
+        <div className='row row-cols-1 row-cols-sm-2 row-cols-md-6'>
+            {block}
+        </div>
+        </>
+}
+
+const List = ({ inputFormat }) => {
+    const list = SUPPORTED.map((outputFormat) => inputFormat !== outputFormat ? <Element inputFormat={inputFormat} outputFormat={outputFormat} /> : null)
+
+    return <div className="col mb-3 align-items-end text-end">
+        <ul className="nav flex-column ">
+            { list }
+            <li className="nav-item mb-2">
+                <Link to={`/${inputFormat}`} className="nav-link p-0 text-body-secondary">{`${firstToUpper(inputFormat)} Format`}</Link>
+            </li>
+        </ul>
+    </div>
+}
+
+const Element = ({ inputFormat, outputFormat }) => {
+    return <li className="nav-item mb-2">
+        <Link to={`/${inputFormat}/${outputFormat}`} className="nav-link p-0 text-body-secondary">{`${firstToUpper(inputFormat)} to ${firstToUpper(outputFormat)}`}</Link>
+    </li>
+}
+
+export default ConverterMenu
--- src/components/Footer/footer.js
+++ src/components/Footer/footer.js
@@ -1,162 +1,24 @@
 import Logo from '../Logo';
 import { Link } from 'react-router-dom';
+import ConverterMenu from "./ConverterMenu";
+
 
 const Footer = () => {
     const year = new Date().getFullYear();
     return <footer className="py-3">
         <div className="container-fluid">
             <div className="row">
-                <div className="col-2 p-3">
-                </div>
-                <div className="col-8 p3">
-                    <div className='row row-cols-1 row-cols-sm-2 row-cols-md-6'>
-                        {/*<div className="col mb-3 align-items-center">*/}
-                            {/*<a href="/"*/}
-                            {/*   className="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none"*/}
-                            {/*   aria-label="Bootstrap">*/}
-                            {/*    <Logo height='64' img='icon'/>*/}
-                            {/*</a>*/}
-                        {/*</div>*/}
-
-                        <div className="col mb-3 align-items-end text-end">
-                            <ul className="nav flex-column ">
-                                <li className="nav-item mb-2">
-                                    <Link to="/json/xml" className="nav-link p-0 text-body-secondary">Json to Xml</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/json/yaml" className="nav-link p-0 text-body-secondary">Json to Yaml</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/json/csv" className="nav-link p-0 text-body-secondary">Json to Csv</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/json/toml" className="nav-link p-0 text-body-secondary">Json to Toml</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/json/tsv" className="nav-link p-0 text-body-secondary">Json to Tsv</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/json" className="nav-link p-0 text-body-secondary">Json Format</Link>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="col mb-3 align-items-end text-end">
-                            <ul className="nav flex-column ">
-                                <li className="nav-item mb-2">
-                                    <Link to="/xml/json" className="nav-link p-0 text-body-secondary">Xml to Json</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/xml/yaml" className="nav-link p-0 text-body-secondary">Xml to Yaml</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/xml/csv" className="nav-link p-0 text-body-secondary">Xml to Csv</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/xml/toml" className="nav-link p-0 text-body-secondary">Xml to Toml</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/xml/tsv" className="nav-link p-0 text-body-secondary">Xml to Tsv</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/xml" className="nav-link p-0 text-body-secondary">Xml Format</Link>
-                                </li>
-                            </ul>
-                        </div>
-                        <div className="col mb-3 align-items-end text-end">
-                            <ul className="nav flex-column ">
-                                <li className="nav-item mb-2">
-                                    <Link to="/yaml/json" className="nav-link p-0 text-body-secondary">Yaml to Json</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/yaml/xml" className="nav-link p-0 text-body-secondary">Yaml to Xml</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/yaml/csv" className="nav-link p-0 text-body-secondary">Yaml to Csv</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/yaml/toml" className="nav-link p-0 text-body-secondary">Yaml to Toml</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/yaml/tsv" className="nav-link p-0 text-body-secondary">Yaml to Tsv</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/yaml" className="nav-link p-0 text-body-secondary">Yaml Format</Link>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div className="col mb-3 align-items-end text-end">
-                            <ul className="nav flex-column ">
-                                <li className="nav-item mb-2">
-                                    <Link to="/csv/json" className="nav-link p-0 text-body-secondary">Csv to Json</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/csv/xml" className="nav-link p-0 text-body-secondary">Csv to Xml</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/csv/yaml" className="nav-link p-0 text-body-secondary">Csv to Yaml</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/csv/toml" className="nav-link p-0 text-body-secondary">Csv to Toml</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/csv/tsv" className="nav-link p-0 text-body-secondary">Csv to Tsv</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/csv" className="nav-link p-0 text-body-secondary">Csv Format</Link>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div className="col mb-3 align-items-end text-end">
-                            <ul className="nav flex-column ">
-                                <li className="nav-item mb-2">
-                                    <Link to="/toml/json" className="nav-link p-0 text-body-secondary">Toml to Json</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/toml/xml" className="nav-link p-0 text-body-secondary">Toml to Xml</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/toml/yaml" className="nav-link p-0 text-body-secondary">Toml to Yaml</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/toml/csv" className="nav-link p-0 text-body-secondary">Toml to Csv</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/toml/tsv" className="nav-link p-0 text-body-secondary">Toml to Tsv</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/toml" className="nav-link p-0 text-body-secondary">Toml Format</Link>
-                                </li>
-                            </ul>
-                        </div>
-
-                        <div className="col mb-3 align-items-end text-end">
-                            <ul className="nav flex-column ">
-                                <li className="nav-item mb-2">
-                                    <Link to="/tsv/json" className="nav-link p-0 text-body-secondary">Tsv to Json</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/tsv/xml" className="nav-link p-0 text-body-secondary">Tsv to Xml</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/tsv/yaml" className="nav-link p-0 text-body-secondary">Tsv to Yaml</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/tsv/csv" className="nav-link p-0 text-body-secondary">Tsv to Csv</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/tsv/toml" className="nav-link p-0 text-body-secondary">Tsv to Toml</Link>
-                                </li>
-                                <li className="nav-item mb-2">
-                                    <Link to="/tsv" className="nav-link p-0 text-body-secondary">Tsv Format</Link>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+                <div className="col-10 p3">
+                    <ConverterMenu/>
                 </div>
                 <div className="col-2 p3">
-
+                    {/*<div className="col mb-3 align-items-center">*/}
+                    {/*    <a href="/"*/}
+                    {/*       className="d-flex align-items-center mb-3 link-body-emphasis text-decoration-none"*/}
+                    {/*       aria-label="Bootstrap">*/}
+                    {/*        <Logo height='64' img='icon'/>*/}
+                    {/*    </a>*/}
+                    {/*</div>*/}
                 </div>
             </div>
         </div>
